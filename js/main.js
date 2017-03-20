2b6b548060549d85fa7d021255918f731b1e70a2
Spencer Bouse <Orangesrb@gmail.com>
2017-03-20 16:20:39 -0400

    upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$('#city').submit(function(e){
+  e.preventDefault();
+  var name = $('#cityInput').val();
+  var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q='+ name +'&cnt=7&units=imperial&APPID=d0458c4189cf033bf80c84d7a0d38ab0'
+  $('#cityInput').val('');
+  $('#weather').html('');
+
+$.ajax({url: url,
+  success: function(response){
+    console.log(response);
+    response.list.forEach(function(day){
+      console.log(day);
+      $('#weather').append( `
+        <div class="divide col-md-1">
+          <h5>${day.weather[0].description}</h5>
+          <p>Max Temp: ${day.temp.max}</p>
+          <p>Min Temp:${day.temp.min}</p>
+        </div>
+        `);
+  });
+  }})
+})
